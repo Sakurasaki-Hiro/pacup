be93d1228ad47fa7e74b8f82f25a17e9e586e10d
Syuugo <pub@s1204.me>
2022-10-26 16:52:00 +0900

    大幅アップデート (#2)
    * Major Update
    
    * Support WSL
    
    * `snap` require Root
    
    * Change the way `sed`

--- install_pacup.sh
+++ install_pacup.sh
@@ -1,12 +1,35 @@
 #!/bin/bash
-if [ `whoami` != 'root' ]; then
-  echo "管理者権限で実行してください"
-  exit
+
+if [ ! -f /etc/debian_version ];then
+  echo -e "このディストリビューションには､ \"pacup\" をインストール出来ません｡\n\nDebian系 もしくは Debian派生系のみ対応しています｡"
+  exit 1
 fi
 
-sudo cp -f ./pacup.sh /usr/local/bin/pacup
-sudo chmod +x /usr/local/bin/pacup
-echo "Success!"
+if [ `whoami` != 'root' ]; then
+  sudo $BASH_SOURCE
+  if [ $? != 0 ]; then
+    echo "Root(管理者)権限で実行してください"
+    exit 1
+  fi
+  exit 0
+fi
 
-exit
-Footer
+if [ ! -f ./pacup ]; then
+  echo -e "\"pacup\" ファイルが見つかりませんでした\n"
+  exit 1
+fi
+
+command -v snap >/dev/null 2>&1
+if [ $? == 0 ] && [ ! -f /usr/bin/wslfetch ]; then
+   sed -i -e "/^  : #SYS$/a \ \ sudo snap refresh" ./pacup
+fi
+command -v flatpak >/dev/null 2>&1
+if [ $? == 0 ]; then
+   sed -i -e "s/#FPK/flatpak update/g" ./pacup
+   sed -i -e "/^  : #SYS$/a \ \ sudo flatpak update" ./pacup
+fi
+
+cp -f ./pacup /usr/local/bin/pacup
+chmod +rx /usr/local/bin/pacup
+echo "Success!"
+exit 0
